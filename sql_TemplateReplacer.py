7194eff8e9ba892d2855a00482b46e645ecfb047
Andrey Baranov <dronecz@gmail.com>
2022-12-12 17:08:02 +1100

    Update sql_TemplateReplacer.py

--- sql_TemplateReplacer.py
+++ sql_TemplateReplacer.py
@@ -1,8 +1,15 @@
 fields = ['AssetNo'
       ,'WorkOrder'
       ,'DateCompleted'
-      ]
+      ,'FromPoint'
+      ,'ToPoint'
+      ,'Note']
 output = "@template"
+outputxml = ""
+c = 1
 for field in fields:
     output = f"REPLACE({output},'%{field}%',{field})"
+    outputxml = outputxml+f'<PromptData sequence="{c}">%{field}%</PromptData>\n'
+    c=c+1
 print(output)
+print(outputxml)
